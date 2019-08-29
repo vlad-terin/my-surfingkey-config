868d9657ddc56f5fef38e82344aec3c7471734c9
Vlad Terin <vladterin@gmail.com>
2019-08-29 17:43:53 -0500

    Update surfingkeys-config.js

--- surfingkeys-config.js
+++ surfingkeys-config.js
@@ -5,13 +5,14 @@ imap("<Ctrl-w>","Alt-w>");
 imap('jj', "<Esc>");
 map('sU', 'su');
 
-{
-  alias:       "yI",
-  category:    categories.clipboard,
-  description: "Copy Image URL",
-  callback:    actions.createHint("img", i => Clipboard.write(i.src)),
+const maps = {
+    {
+      alias:       "yI",
+      category:    categories.clipboard,
+      description: "Copy Image URL",
+      callback:    actions.createHint("img", i => Clipboard.write(i.src)),
+    }
 }
-
 // Youtube Fullscreen, credit github.com/okiptkn/dotfiles
 function ytFullscreen() {
   $(".ytp-fullscreen-button.ytp-button").click()
