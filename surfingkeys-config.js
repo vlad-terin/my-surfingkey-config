909dfddb1cd333ce980eaa48d83124636a6caaef
vlad-terin <vladterin@gmail.com>
2020-12-02 13:49:23 -0600

    solarized theme

--- surfingkeys-config.js
+++ surfingkeys-config.js
@@ -1092,35 +1092,60 @@ mapkey("gT", "Goto Text Area", function () {
   documents.getElementsByClassName(".textarea")[0].click();
 });
 
-// gruvbox
+// solarized dark
 settings.theme = `
 .sk_theme {
-    font-family: Input Sans Condensed, Charcoal, sans-serif;
-    font-size: 14pt;
-    background: #282828;
-    color: #ebdbb2;
-}
-.sk_theme tbody {
-    color: #b8bb26;
+  font-family: Input Sans Condensed, Charcoal, sans-serif;
+  font-size: 10pt;
+  background: #002B36;
+  color: #93A1A1;
 }
 .sk_theme input {
-    color: #d9dce0;
+  color: #93A1A1;
 }
 .sk_theme .url {
-    color: #98971a;
+  color: #268BD2;
 }
 .sk_theme .annotation {
-    color: #b16286;
+  color: #93A1A1;
+}
+.sk_theme kbd {
+  background: #EEE8D5;
+  color: #111;
 }
 .sk_theme .omnibar_highlight {
-    color: #ebdbb2;
+  color: #CB4B16;
+}
+.sk_theme .omnibar_folder {
+  color: #2AA198;
+}
+.sk_theme .omnibar_timestamp {
+  color: #657B83;
+}
+.sk_theme .omnibar_visitcount {
+  color: #B58900;
+}
+.sk_theme .prompt, .sk_theme .resultPage {
+  color: #93A1A1;
+}
+.sk_theme .feature_name {
+  color: #859900;
+}
+.sk_theme .separator {
+  color: #859900;
 }
 .sk_theme #sk_omnibarSearchResult ul li:nth-child(odd) {
-    background: #282828;
+  background: #002F3B;
 }
 .sk_theme #sk_omnibarSearchResult ul li.focused {
-    background: #d3869b;
+  background: #083D4A;
 }
 #sk_status, #sk_find {
-    font-size: 20pt;
+  font-size: 12pt;
+}
+#sk_keystroke {
+  background: #002B36;
+}
+.expandRichHints span.annotation {
+  color: #93A1A1;
 }`;
