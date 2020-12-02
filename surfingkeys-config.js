969f37b9b087cd4ea8d3ced88eda3049c03b6b09
vlad-terin <vladterin@gmail.com>
2020-12-02 11:33:25 -0600

    added new theme

--- surfingkeys-config.js
+++ surfingkeys-config.js
@@ -172,103 +172,110 @@ unmap("op");
   const googleSearchQ = "https://www.google.com/search?q=",
     googleSearchBase = googleSearchQ + "{0}",
     tStr = googleSearchBase + "&tbs=qdr:",
+    // hackernews
     hackernewsBase = "https://hn.algolia.com/?q={0}&page=0&prefix=false",
     hnTStr = hackernewsBase + "&dateRange=",
+    // product hunt
     producthuntBase = "https://www.producthunt.com/search?q={0}",
     phTStr = producthuntBase + "&postedDate=",
-    searches = {
-      pH: ["product hunt", producthuntBase],
-      phy: ["product hunt year", phTStr + "12%3Amonths"],
-      ph3m: ["product hunt year", phTStr + "90%3Adays"],
-      phm: ["product hunt year", phTStr + "30%3Adays"],
-      // search
-      hN: ["hacker news", hackernewsBase],
-      hny: ["hacker news year", hnTStr + "pastYear"],
-      hnm: ["hacker news month", hnTStr + "pastMonth"],
-      hnw: ["hacker news week", hnTStr + "pastWeek"],
-      hnd: ["hacker news day", hnTStr + "last24h"],
+    // github code
+    githubBaseRepo = "https://github.com/search?q={0}&type=repositories";
+  githubBaseCode = "https://github.com/search?o=desc&q={0}&s=&type=Code";
+  githubBaseCommits = "https://github.com/search?q={0}&type=commits";
+  githubBaseIssues = "https://github.com/search?q={0}&type=issues";
 
-      G: ["Google", googleSearchBase],
-      gm: ["googlemonth", tStr + "m"],
-      gw: ["googleweek", tStr + "w"],
-      gy: ["googleyear", tStr + "y"],
-      gd: ["googleday", tStr + "d"],
-      gh: ["googlehour", tStr + "h"],
+  (searches = {
+    // search
 
-      // conflict, changed from l to ll
-      ll: ["lucky", googleSearchBase + "&btnI"],
-      // conflict, changed from t to tt
-      tt: ["onelook", "https://www.onelook.com/?w={0}&ls=a"],
-      s: ["onelook synonyms", "https://www.onelook.com/thesaurus/?s={0}"],
-      d: [
-        "google drive search",
-        "https://drive.google.com/drive/u/1/search?q={0}",
-      ],
-      // conflict, changed from c to cc
-      cc: ["technical translation", "https://techterms.com/definition/{0}"],
-      // conflict, changed from w to ww to wk
-      wk: ["wiki", "https://en.wikipedia.org/wiki/{0}"],
+    pH: ["product hunt", producthuntBase],
+    phy: ["product hunt year", phTStr + "12%3Amonths"],
+    ph3m: ["product hunt year", phTStr + "90%3Adays"],
+    phm: ["product hunt year", phTStr + "30%3Adays"],
 
-      // conflict, changed from p to ppp
-      ppp: ["duckHTML", "https://duckduckgo.com/html/?q={0}"],
+    hN: ["hacker news", hackernewsBase],
+    hny: ["hacker news year", hnTStr + "pastYear"],
+    hnm: ["hacker news month", hnTStr + "pastMonth"],
+    hnw: ["hacker news week", hnTStr + "pastWeek"],
+    hnd: ["hacker news day", hnTStr + "last24h"],
 
-      //map
-      gM: ["google maps", "https://www.google.com/maps?q="],
+    gh: ["github", githubBaseRepo],
 
-      //coding
-      C: ["search coding", "https://searchcode.com/?q="],
-      cC: ["search coding", "https://searchcode.com/?q="],
-      cw: ["chrome webstore", "https://chrome.google.com/webstore/search/"], // chrome
-      cS: ["slant (editor 비교 사이트)", "https://www.slant.co/search?query="],
-      gH: ["github", "https://github.com/search?q="],
-      ghS: [
-        "githubStars",
-        "https://github.com/vlad-terin?page=1&q=face&tab=stars&utf8=%E2%9C%93&utf8=%E2%9C%93&q=",
-      ],
-      gC: ["githubCode", "https://github.com/search?q={0}&type=Code"],
+    G: ["Google", googleSearchBase],
+    gm: ["googlemonth", tStr + "m"],
+    gw: ["googleweek", tStr + "w"],
+    gy: ["googleyear", tStr + "y"],
+    gd: ["googleday", tStr + "d"],
+    gh: ["googlehour", tStr + "h"],
 
-      //language
-      lJ: ["language Javascript", googleSearchQ + "Javascript+"],
-      lj: ["language java", googleSearchQ + "Java+"],
-      lC: ["C++", googleSearchQ + "C++"],
-      lc: ["language c", googleSearchQ + "c+language+"],
-      "l#": ["language C#", googleSearchQ + "c%23+"],
-      lR: ["language R", googleSearchQ + "languag+"],
-      lr: ["language Ruby", googleSearchQ + "Ruby+"],
-      lP: ["language Python", googleSearchQ + "Python+"],
-      lp: ["language php", googleSearchQ + "php+"],
-      lK: ["language Kotlin", googleSearchQ + "Kotlin+"],
-      lS: ["language Swift", googleSearchQ + "Swift+"],
-      lQ: ["language SQL Query", googleSearchQ + "SQL+"],
-      ls: ["language Shell script", googleSearchQ + "Shell+Schript+"],
-      lT: ["language Typescript", googleSearchQ + "TypeScript+"],
-      lH: ["language HTML", googleSearchQ + "HTML+"],
+    // conflict, changed from l to ll
+    ll: ["lucky", googleSearchBase + "&btnI"],
+    // conflict, changed from t to tt
+    tt: ["onelook", "https://www.onelook.com/?w={0}&ls=a"],
+    s: ["onelook synonyms", "https://www.onelook.com/thesaurus/?s={0}"],
+    d: [
+      "google drive search",
+      "https://drive.google.com/drive/u/1/search?q={0}",
+    ],
+    // conflict, changed from c to cc
+    cc: ["technical translation", "https://techterms.com/definition/{0}"],
+    // conflict, changed from w to ww to wk
+    wk: ["wiki", "https://en.wikipedia.org/wiki/{0}"],
 
-      //sns
-      fb: ["faceBook(페이스북)", "https://www.facebook.com/search/top/?q="],
-      tw: ["tWitter", "https://twitter.com/search?q="],
-      ig: ["InstaGram HashTag", "https://www.instagram.com/explore/tags/"],
-      rd: ["redDit", "https://www.reddit.com/search?q="],
+    // conflict, changed from p to ppp
+    ppp: ["duckHTML", "https://duckduckgo.com/html/?q={0}"],
 
-      //shorten - what is.. who is.. where is..
-      wa: ["advanced", googleSearchQ + "advanced+"],
-      wb: ["basic", googleSearchQ + "basic+"],
-      wc: ["classification", googleSearchQ + "classfication+of+"],
-      wd: ["difference", googleSearchQ + "difference+between+"],
-      we: ["example", googleSearchQ + "example+of+"],
-      ww: ["wherefrom", googleSearchQ + "where+from+"],
-      wg: ["goalof", googleSearchQ + "what+is+goal+of+"],
-      wh: ["historyof", googleSearchQ + "history+of+"],
-      wi: ["introductionof", googleSearchQ + "Introduction+of"],
+    //map
+    gM: ["google maps", "https://www.google.com/maps?q="],
 
-      //file
-      pdf: ["fppdf", googleSearchQ + "filetype%3Apdf+"],
-      cpp: ["fpcpp", googleSearchQ + "filetype%3Acpp+"],
-      hwp: ["fphwp", googleSearchQ + "filetype%3Ahwp+"],
-      ppt: ["fpppt", googleSearchQ + "filetype%3Appt+"],
-    },
+    //coding
+    C: ["search coding", "https://searchcode.com/?q="],
+    cC: ["search coding", "https://searchcode.com/?q="],
+    cw: ["chrome webstore", "https://chrome.google.com/webstore/search/"], // chrome
+    cS: ["slant (editor 비교 사이트)", "https://www.slant.co/search?query="],
+    gH: ["github", "https://github.com/search?q="],
+
+    //language
+    lJ: ["language Javascript", googleSearchQ + "Javascript+"],
+    lj: ["language java", googleSearchQ + "Java+"],
+    lC: ["C++", googleSearchQ + "C++"],
+    lc: ["language c", googleSearchQ + "c+language+"],
+    "l#": ["language C#", googleSearchQ + "c%23+"],
+    lR: ["language R", googleSearchQ + "languag+"],
+    lr: ["language Ruby", googleSearchQ + "Ruby+"],
+    lP: ["language Python", googleSearchQ + "Python+"],
+    lp: ["language php", googleSearchQ + "php+"],
+    lK: ["language Kotlin", googleSearchQ + "Kotlin+"],
+    lS: ["language Swift", googleSearchQ + "Swift+"],
+    lQ: ["language SQL Query", googleSearchQ + "SQL+"],
+    ls: ["language Shell script", googleSearchQ + "Shell+Schript+"],
+    lT: ["language Typescript", googleSearchQ + "TypeScript+"],
+    lH: ["language HTML", googleSearchQ + "HTML+"],
+
+    //sns
+    fb: ["faceBook(페이스북)", "https://www.facebook.com/search/top/?q="],
+    tw: ["tWitter", "https://twitter.com/search?q="],
+    ig: ["InstaGram HashTag", "https://www.instagram.com/explore/tags/"],
+    rd: ["redDit", "https://www.reddit.com/search?q="],
+
+    //shorten - what is.. who is.. where is..
+    wa: ["advanced", googleSearchQ + "advanced+"],
+    wb: ["basic", googleSearchQ + "basic+"],
+    wc: ["classification", googleSearchQ + "classfication+of+"],
+    wd: ["difference", googleSearchQ + "difference+between+"],
+    we: ["example", googleSearchQ + "example+of+"],
+    ww: ["wherefrom", googleSearchQ + "where+from+"],
+    wg: ["goalof", googleSearchQ + "what+is+goal+of+"],
+    wh: ["historyof", googleSearchQ + "history+of+"],
+    wi: ["introductionof", googleSearchQ + "Introduction+of"],
+
+    //file
+    pdf: ["fppdf", googleSearchQ + "filetype%3Apdf+"],
+    cpp: ["fpcpp", googleSearchQ + "filetype%3Acpp+"],
+    hwp: ["fphwp", googleSearchQ + "filetype%3Ahwp+"],
+    ppt: ["fpppt", googleSearchQ + "filetype%3Appt+"],
+  }),
     // only 's' and 'o' prefixed mappings will be taken
-    searchGroups = {
+    (searchGroups = {
       "0": {
         name: "Social networks searches",
         list: ["fb", "tw", "ig", "rd"],
@@ -285,7 +292,7 @@ unmap("op");
         name: "Product Hunt Combined",
         list: ["phy", "ph3m", "phm"],
       },
-    };
+    });
 
   // register mappings for searches
   for (let k in searches) {
@@ -1082,303 +1089,35 @@ mapkey("gT", "Goto Text Area", function () {
   documents.getElementsByClassName(".textarea")[0].click();
 });
 
-//credit https://github.com/Foldex/surfingkeys-config/blob/master/themes.js
-
-// ---- Hints ----
-// Hints have to be defined separately
-// Uncomment to enable
-
-// Tomorrow-Night
-
-Hints.style(
-  "border: solid 2px #373B41; color:#52C196; background: initial; background-color: #1D1F21;"
-);
-Hints.style(
-  "border: solid 2px #373B41 !important; padding: 1px !important; color: #C5C8C6 !important; background: #1D1F21 !important;",
-  "text"
-);
-Visual.style("marks", "background-color: #52C19699;");
-Visual.style("cursor", "background-color: #81A2BE;");
-
-// Nord
-/* -- DELETE LINE TO ENABLE THEME
-Hints.style('border: solid 2px #4C566A; color:#A3BE8C; background: initial; background-color: #3B4252;');
-Hints.style('border: solid 2px #4C566A !important; padding: 1px !important; color: #E5E9F0 !important; background: #3B4252 !important;', 'text');
-Visual.style('marks', 'background-color: #A3BE8C99;');
-Visual.style('cursor', 'background-color: #88C0D0;');
--- DELETE LINE TO ENABLE THEME */
-
-// Doom One
-/* -- DELETE LINE TO ENABLE THEME
-Hints.style('border: solid 2px #282C34; color:#98be65; background: initial; background-color: #2E3440;');
-Hints.style('border: solid 2px #282C34 !important; padding: 1px !important; color: #51AFEF !important; background: #2E3440 !important;', 'text');
-Visual.style('marks', 'background-color: #98be6599;');
-Visual.style('cursor', 'background-color: #51AFEF;');
--- DELETE LINE TO ENABLE THEME */
-
-// Monokai
-/* -- DELETE LINE TO ENABLE THEME
-Hints.style('border: solid 2px #2D2E2E; color:#F92660; background: initial; background-color: #272822;');
-Hints.style('border: solid 2px #2D2E2E !important; padding: 1px !important; color: #A6E22E !important; background: #272822 !important;', 'text');
-Visual.style('marks', 'background-color: #A6E22E99;');
-Visual.style('cursor', 'background-color: #F92660;');
--- DELETE LINE TO ENABLE THEME */
-
+// gruvbox
 settings.theme = `
-/* Edit these variables for easy theme making */
-:root {
-  /* Font */
-  --font: 'Source Code Pro', Ubuntu, sans;
-  --font-size: 16;
-  --font-weight: normal;
-  /* -------------- */
-  /* --- THEMES --- */
-  /* -------------- */
-  /* -------------------- */
-  /* -- Tomorrow Night -- */
-  /* -------------------- */
-
-  /* -- DELETE LINE TO ENABLE THEME
-  --fg: #C5C8C6;
-  --bg: #282A2E;
-  --bg-dark: #1D1F21;
-  --border: #373b41;
-  --main-fg: #81A2BE;
-  --accent-fg: #52C196;
-  --info-fg: #AC7BBA;
-  --select: #585858;
-  -- DELETE LINE TO ENABLE THEME */
-  /* Unused Alternate Colors */
-  /* --cyan: #4CB3BC; */
-  /* --orange: #DE935F; */
-  /* --red: #CC6666; */
-  /* --yellow: #CBCA77; */
-  /* -------------------- */
-  /* --      NORD      -- */
-  /* -------------------- */
-  /* -- DELETE LINE TO ENABLE THEME
-  --fg: #E5E9F0;
-  --bg: #3B4252;
-  --bg-dark: #2E3440;
-  --border: #4C566A;
-  --main-fg: #88C0D0;
-  --accent-fg: #A3BE8C;
-  --info-fg: #5E81AC;
-  --select: #4C566A;
-  -- DELETE LINE TO ENABLE THEME */
-  /* Unused Alternate Colors */
-  /* --orange: #D08770; */
-  /* --red: #BF616A; */
-  /* --yellow: #EBCB8B; */
-  /* -------------------- */
-  /* --    DOOM ONE    -- */
-  /* -------------------- */
-  --fg: #51AFEF;
-  --bg: #2E3440;
-  --bg-dark: #21242B;
-  --border: #FFFFFF;
-  --main-fg: #51AFEF;
-  --accent-fg: #98be65;
-  --info-fg: #C678DD;
-  --select: #4C566A;
-  /* Unused Alternate Colors */
-  /* --bg-dark: #21242B; */
-  /* --main-fg-alt: #2257A0; */
-  /* --cyan: #46D9FF; */
-  /* --orange: #DA8548; */
-  /* --red: #FF6C6B; */
-  /* --yellow: #ECBE7B; */
-  /* -------------------- */
-  /* --    MONOKAI    -- */
-  /* -------------------- */
-  /* -- DELETE LINE TO ENABLE THEME
-  --fg: #F8F8F2;
-  --bg: #272822;
-  --bg-dark: #1D1E19;
-  --border: #2D2E2E;
-  --main-fg: #F92660;
-  --accent-fg: #E6DB74;
-  --info-fg: #A6E22E;
-  --select: #556172;
-  /* Unused Alternate Colors */
-  /* --red: #E74C3C; */
-  /* --orange: #FD971F; */
-  /* --blue: #268BD2; */
-  /* --violet: #9C91E4; */
-  /* --cyan: #66D9EF; */
-  -- DELETE LINE TO ENABLE THEME */
-  /* Unused Alternate Colors */
-}
-/* ---------- Generic ---------- */
 .sk_theme {
-background: var(--bg);
-color: var(--fg);
-  background-color: var(--bg);
-  border-color: var(--border);
-  font-family: var(--font);
-  font-size: var(--font-size);
-  font-weight: var(--font-weight);
-}
-input {
-  font-family: var(--font);
-  font-weight: var(--font-weight);
+    font-family: Input Sans Condensed, Charcoal, sans-serif;
+    font-size: 10pt;
+    background: #282828;
+    color: #ebdbb2;
 }
 .sk_theme tbody {
-  color: var(--fg);
+    color: #b8bb26;
 }
 .sk_theme input {
-  color: var(--fg);
-}
-/* Hints */
-#sk_hints .begin {
-  color: var(--accent-fg) !important;
-}
-#sk_tabs .sk_tab {
-  background: var(--bg-dark);
-  border: 1px solid var(--border);
-  color: var(--fg);
-}
-#sk_tabs .sk_tab_hint {
-  background: var(--bg);
-  border: 1px solid var(--border);
-  color: var(--accent-fg);
-}
-.sk_theme #sk_frame {
-  background: var(--bg);
-  opacity: 0.2;
-  color: var(--accent-fg);
-}
-/* ---------- Omnibar ---------- */
-/* Uncomment this and use settings.omnibarPosition = 'bottom' for Pentadactyl/Tridactyl style bottom bar */
-/* .sk_theme#sk_omnibar {
-  width: 100%;
-  left: 0;
-} */
-// .sk_theme .title {
-  color: var(--accent-fg);
+    color: #d9dce0;
 }
 .sk_theme .url {
-  color: var(--main-fg);
+    color: #98971a;
 }
 .sk_theme .annotation {
-  color: var(--accent-fg);
+    color: #b16286;
 }
 .sk_theme .omnibar_highlight {
-  color: var(--accent-fg);
-}
-.sk_theme .omnibar_timestamp {
-  color: var(--info-fg);
-}
-.sk_theme .omnibar_visitcount {
-  color: var(--accent-fg);
+    color: #ebdbb2;
 }
 .sk_theme #sk_omnibarSearchResult ul li:nth-child(odd) {
-  background: var(--bg-dark);
+    background: #282828;
 }
 .sk_theme #sk_omnibarSearchResult ul li.focused {
-  background: var(--border);
+    background: #d3869b;
 }
-.sk_theme #sk_omnibarSearchArea {
-  border-top-color: var(--border);
-  border-bottom-color: var(--border);
-}
-.sk_theme #sk_omnibarSearchArea input,
-.sk_theme #sk_omnibarSearchArea span {
-  font-size: var(--font-size);
-}
-.sk_theme .separator {
-  color: var(--accent-fg);
-}
-/* ---------- Popup Notification Banner ---------- */
-#sk_banner {
-  font-family: var(--font);
-  font-size: var(--font-size);
-  font-weight: var(--font-weight);
-  background: var(--bg);
-  border-color: var(--border);
-  color: var(--fg);
-  opacity: 0.9;
-}
-/* ---------- Popup Keys ---------- */
-#sk_keystroke {
-  background-color: var(--bg);
-}
-.sk_theme kbd .candidates {
-  color: var(--info-fg);
-}
-.sk_theme span.annotation {
-  color: var(--accent-fg);
-}
-/* ---------- Popup Translation Bubble ---------- */
-#sk_bubble {
-  background-color: var(--bg) !important;
-  color: var(--fg) !important;
-  border-color: var(--border) !important;
-}
-#sk_bubble * {
-  color: var(--fg) !important;
-}
-#sk_bubble div.sk_arrow div:nth-of-type(1) {
-  border-top-color: var(--border) !important;
-  border-bottom-color: var(--border) !important;
-}
-#sk_bubble div.sk_arrow div:nth-of-type(2) {
-  border-top-color: var(--bg) !important;
-  border-bottom-color: var(--bg) !important;
-}
-/* ---------- Search ---------- */
-#sk_status,
-#sk_find {
-  font-size: var(--font-size);
-  border-color: var(--border);
-}
-.sk_theme kbd {
-  background: var(--bg-dark);
-  border-color: var(--border);
-  box-shadow: none;
-  color: var(--fg);
-}
-.sk_theme .feature_name span {
-  color: var(--main-fg);
-}
-/* ---------- ACE Editor ---------- */
-#sk_editor {
-  background: var(--bg-dark) !important;
-  height: 50% !important;
-  /* Remove this to restore the default editor size */
-}
-.ace_dialog-bottom {
-  border-top: 1px solid var(--bg) !important;
-}
-.ace-chrome .ace_print-margin,
-.ace_gutter,
-.ace_gutter-cell,
-.ace_dialog {
-  background: var(--bg) !important;
-}
-.ace-chrome {
-  color: var(--fg) !important;
-}
-.ace_gutter,
-.ace_dialog {
-  color: var(--fg) !important;
-}
-.ace_cursor {
-  color: var(--fg) !important;
-}
-.normal-mode .ace_cursor {
-  background-color: var(--fg) !important;
-  border: var(--fg) !important;
-  opacity: 0.7 !important;
-}
-.ace_marker-layer .ace_selection {
-  background: var(--select) !important;
-}
-.ace_editor,
-.ace_dialog span,
-.ace_dialog input {
-  font-family: var(--font);
-  font-size: var(--font-size);
-  font-weight: var(--font-weight);
-}
-`;
+#sk_status, #sk_find {
+    font-size: 20pt;
+}`;
