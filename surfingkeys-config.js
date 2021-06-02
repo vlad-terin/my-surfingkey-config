68f1d848fd34c41b129c38fe7717dc56ec175b1c
Vlad Terin <vladterin@gmail.com>
2021-06-01 21:52:57 -0500

    Update surfingkeys-config.js

--- surfingkeys-config.js
+++ surfingkeys-config.js
@@ -1,3 +1,37 @@
+
+if (window.origin === "https://www.google.com") {
+    function cycleGoogleSuggestions(forward) {
+        var suggestions = document.querySelectorAll("ul>li.sbct");
+        var selected = document.querySelector("ul>li.sbct.sbhl");
+        var next;
+        if (selected) {
+            selected.classList.remove("sbhl");
+            var next = Array.from(suggestions).indexOf(selected) + (forward ? 1 : -1);
+            if (next === suggestions.length || next === -1) {
+                next = {innerText: window.userInput};
+            } else {
+                next = suggestions[next];
+                next.classList.add("sbhl");
+            }
+        } else {
+            window.userInput = document.querySelector("input.gsfi").value;
+            next = forward ? suggestions[0] : suggestions[suggestions.length - 1];
+            next.classList.add("sbhl");
+        }
+        document.querySelector("input.gsfi").value = next.innerText;
+    }
+    imapkey('<Ctrl-p>', 'cycle google suggestions', function () {
+        cycleGoogleSuggestions(false);
+    });
+    imapkey('<Ctrl-n>', 'cycle google suggestions', function () {
+        cycleGoogleSuggestions(true);
+    });
+    mapkey('zzzz', 'first google result', function() {
+        var cssSelector = '#search h1+div>div:first-child>div:first-child a';
+        // open browser console (F12) on page with search engine results to test CSS selector you build from left to right (look at elements tree)
+    document.querySelector(cssSelector).click();
+    });
+}
 map("L", "R");
 map("H", "E");
 vmap("v", "zv");
