23303c41289216c60a73d0d5b98a53ab315e2527
Vlad <vlad@mac.attlocal.net>
2024-01-08 17:07:02 -0600

    modified

--- surfingkeys-config.js
+++ surfingkeys-config.js
@@ -32,15 +32,15 @@ const API_URL = "https://dictionaryapi.com/api/v3";
 //     alert("No search result link found.");
 //   } });
 //
-api.mapkey("hg", "Go to the first Google result", function () {
+api.mapkey("1h", "Go to the first Google result", function () {
   goToLink(0);
 });
 
-api.mapkey("h2", "Go to the second Google result", function () {
+api.mapkey("2h", "Go to the second Google result", function () {
   goToLink(1);
 });
 
-api.mapkey("h3", "Go to the third Google result", function () {
+api.mapkey("3h", "Go to the third Google result", function () {
   goToLink(2);
 });
 
