e1fe27006b559032b9385cdacdbe6b90b0601ef6
Vlad <vlad@mac.attlocal.net>
2024-01-08 16:39:15 -0600

    another addition

--- surfingkeys-config.js
+++ surfingkeys-config.js
@@ -20,6 +20,18 @@ settings.tabsThreshold = 0;
 const API_KEY = "af712d02-1689-4378-8590-cba02e8341a0";
 const API_URL = "https://dictionaryapi.com/api/v3";
 
+// api.mapkey("h", "Go to the first Google result", function () {
+//   // Selector for the first search result title
+//   var firstResult = document.querySelector(".LC20lb");
+//   // Ensure the element exists and has a parent node which should be the anchor tag
+//   if (firstResult && firstResult.parentNode) {
+//     // Navigate to the href of the parent node
+//     window.location.href = firstResult.parentNode.href;
+//   } else {
+//     // Alert if no result is found
+//     alert("No search result link found.");
+//   } });
+//
 api.mapkey("h", "Go to the first Google result", function () {
   goToLink(0);
 });
@@ -38,12 +50,12 @@ function goToLink(index) {
   // Ensure the selected result exists and has a parent node which should be the anchor tag
   if (results.length > index && results[index].parentNode) {
     // Navigate to the href of the parent node
-    window.location.href = firstResult.parentNode.href;
+    window.location.href = results[index].parentNode.href;
   } else {
-    // Alert if no result is found
-    alert("No search result link found.");
+    // Alert if the specified result is not found
+    alert("No search result link found at position " + (index + 1));
   }
-});
+}
 
 api.Front.registerInlineQuery({
   url: function (q) {
