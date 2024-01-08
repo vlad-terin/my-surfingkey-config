657a45439747310c4d1b27edd23b50c6b4b2a4b7
Vlad <vlad@mac.attlocal.net>
2024-01-08 16:29:23 -0600

    replaced t to h

--- surfingkeys-config.js
+++ surfingkeys-config.js
@@ -20,7 +20,7 @@ settings.tabsThreshold = 0;
 const API_KEY = "af712d02-1689-4378-8590-cba02e8341a0";
 const API_URL = "https://dictionaryapi.com/api/v3";
 
-api.mapkey("t", "Go to the first Google result", function () {
+api.mapkey("h", "Go to the first Google result", function () {
   // Selector for the first search result title
   var firstResult = document.querySelector(".LC20lb");
   // Ensure the element exists and has a parent node which should be the anchor tag
